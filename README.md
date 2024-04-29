fb5224ca8fec943b5561cba573597d4dac9efb72
Humble011 <muhammadhilmi148@gmail.com>
2024-04-29 09:06:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Tentang Developer
+
+Halo, Perkenalkan nama saya Muhammad Hilmi. Saya Mahasiswa semester 6 Prodi Informatika dari Universitas Informatika dan Bisnis Indonesia.
+
+# Deskripsi Singkat Aplikasi
+
+SI-Toktong atau Sistem Informasi Toko Kelontong adalah Aplikasi Penjualan Toko Kelontong yang didalamnya terdapat fitur input barang, transaksi, dan laporan penghasilan. Tujuan dibuatnya aplikasi ini adalah untuk memenuhi penilaian Ujian Tengah Semester mata kuliah Pemrograman Framework, yang diajarkan oleh Ibu Triana Wulandari, M.Kom.
+
+# Tatacara Install dan Pemakaian Aplikasi
+
+- Buat Database di phpmyadmin dengan nama sitoktong
+- Import Database sitoktong.sql ke phpmyadmin
+- Selanjutnya klik file .env untuk mengatur base url nya
+- Sesuaikan base url nya menjadi http://localhost/sitoktong/
+- Akun dummy terdapat dua yaitu akun admin dan karyawan
+- Password kedua akun sama yaitu 12345
+- Klik Get Started pada Landing Page
+- Di halaman login sudah terdapat nama akun yang terdaftar
+- Di nama akun terdapat dropdown untuk memilih akun yang sudah terdaftar
+- Masukkan password 12345
+
+# Fitur Aplikasi Sitoktong:
+
+- Login dan logout.
+  Aplikasi ini sudah memiliki fitur keamanan login dan logout agar tidak digunakan oleh orang lain.
+
+- Management Barang.
+  Aplikasi ini dapat mengatur barang, yaitu menambah jenis barang dan menghapusnya bila ada perubahan atau barang sudah tidak dijual lagi.
+
+- Management User
+  Aplikasi ini dapat digunakan oleh banyak user, dengan jabatan yang berbeda, yaitu admin dan karyawan.
+
+- Transaksi
+  Aplikasi ini dapat digunakan untuk fitur kasir atau transaksi, dimana bisa memilih barang apa saja yang akan dibeli dengan total harga yang ditampilkan.
+
+- Management Stok
+  Stok barang juga bisa diatur di aplikasi ini, bisa menambah atau mengurangi stok secara manual dengan alasan tertentu.
+
+- Laporan Penjualan/keuntungan
+  Laporan penjualan per hari, perbulan, bahkan pertahun dapat dilihat pada aplikasi ini dengan hanya menyesuaikan tanggal yang dibutuhkan saja.
+
+- Jabatan user. karyawan dan admin
+  Jabatan karyawan dan admin ditentukan pada management user, dimana admin dapat menambahkan dan menghapus user, sedangkan karyawan tidak dapat melakukannya.
+
+note: Aplikasi ini belum sepenuh nya responsive, jadi harap dibukanya di desktop/laptop :)
---
+++ app/Views/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Template Mo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="<?php echo base_url() ?>/public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <title>SI-Toktong</title>
+    <!--
+
+ART FACTORY
+
+https://templatemo.com/tm-537-art-factory
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/public/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/public/assets/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/public/assets/css/templatemo-art-factory.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/public/assets/css/owl-carousel.css">
+
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#" class="logo"><i class="fas fa-store"></i>SI-Toktong</a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#welcome" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about">About</a></li>
+
+                            <li class="scroll-to-section"><a href="#contact-us">Contact Us</a></li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+
+    <!-- ***** Welcome Area Start ***** -->
+    <div class="welcome-area" id="welcome">
+
+        <!-- ***** Header Text Start ***** -->
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="left-text col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                        <h1>Sistem Informasi Toko Kelontong</h1>
+                        <p>SI-Toktong adalah Aplikasi Penjualan Toko Kelontong yang didalam nya terdapat fitur input barang, transaksi, dan laporan penghasilan.</p>
+                        <a href="<?= base_url('login') ?>"" class=" main-button-slider">Get Started</a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                        <img src="<?= base_url() ?>/public/assets/images/slider-icon.svg" class="rounded img-fluid d-block mx-auto" style="max-width: 350px; max-height: 350px;" alt="First Vector Graphic">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ***** Header Text End ***** -->
+    </div>
+    <!-- ***** Welcome Area End ***** -->
+
+
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="<?= base_url() ?>/public/assets/images/hilmi.jpeg" class="rounded-circle img-fluid d-block mx-auto img-thumbnail" style="max-width: 350px; max-height: 350px;" alt="App">
+                </div>
+                <div class="right-text col-lg-5 col-md-12 col-sm-12 mobile-top-fix">
+                    <div class="left-heading">
+                        <h5>Tentang Aplikasi Ini</h5>
+                    </div>
+                    <div class="left-text text-justify">
+                        <p>Halo, Perkenalkan nama saya Muhammad Hilmi.
+                            Saya Mahasiswa semester 6 Prodi Informatika dari
+                            Universitas Informatika dan Bisnis Indonesia.
+                            Tujuan di buatnya aplikasi ini untuk memenuhi penilaian Ujian Tengah Semester mata kuliah Pemrograman Framework,
+                            yang di ampu oleh Ibu Triana Wulandari, M.Kom .
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Big Item End ***** -->
+
+    <!-- *****Contact Us Start ***** -->
+    <section class="section" id="contact-us" style="margin-top: 100px;">
+        <div class="container">
+            <div class="row">
+                <!-- ***** Contact Map Start ***** -->
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div id="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.565922497918!2d107.64804407499669!3d-6.942366043057683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c2d834ee57cb%3A0xf097d99bcccc4293!2sUniversitas%20Informatika%20Dan%20Bisnis%20Indonesia%20(UNIBI)!5e0!3m2!1sid!2sid!4v1714298759556!5m2!1sid!2sid" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+                <!-- ***** Contact Map End ***** -->
+
+                <!-- ***** Contact Form Start ***** -->
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="contact-form">
+                        <form id="contact" action="" method="post">
+                            <h1 class="text-white mb-5">Contact Us</h1>
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12">
+                                    <fieldset>
+                                        <input name="name" type="text" id="name" placeholder="Full Name" required="" class="contact-field">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <fieldset>
+                                        <input name="email" type="text" id="email" placeholder="E-mail" required="" class="contact-field">
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        <textarea name="message" rows="6" id="message" placeholder="Your Message" required="" class="contact-field"></textarea>
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        <button type="submit" id="form-submit" class="main-button">Send It</button>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- ***** Contact Form End ***** -->
+            </div>
+        </div>
+    </section>
+    <!-- ***** Contact Us End ***** -->
+
+
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12">
+                    <p class="copyright">Copyright &copy; 2024 SI-Toktong
+
+
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12">
+                    <ul class="social">
+                        <li><a href="https://www.instagram.com/muhammadhilmi011/"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="https://github.com/Humble011"><i class="fa fa-github"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/muhammad-hilmi-07017425b/"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?= base_url() ?>/public/assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="<?= base_url() ?>/public/assets/js/popper.js"></script>
+    <script src="<?= base_url() ?>/public/assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="<?= base_url() ?>/public/assets/js/owl-carousel.js"></script>
+    <script src="<?= base_url() ?>/public/assets/js/scrollreveal.min.js"></script>
+    <script src="<?= base_url() ?>/public/assets/js/waypoints.min.js"></script>
+    <script src="<?= base_url() ?>/public/assets/js/jquery.counterup.min.js"></script>
+    <script src="<?= base_url() ?>/public/assets/js/imgfix.min.js"></script>
+
+    <!-- Global Init -->
+    <script src="<?= base_url() ?>/public/assets/js/custom.js"></script>
+
+</body>
+
+</html>
